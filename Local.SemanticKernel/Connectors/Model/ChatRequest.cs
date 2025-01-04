b5fd6474be4f6bf5b97b74bdb3a1130ed7e4a547
Jon Lipsky <jon@elevenworks.com>
2025-01-03 20:34:55 -0800

    Refactored directory structure.

--- Local.SemanticKernel/Connectors/Model/ChatRequest.cs
+++ Local.SemanticKernel/Connectors/Model/ChatRequest.cs
@@ -8,18 +8,33 @@ namespace Local.SemanticKernel.Connectors.Model;
 /// </summary>
 public class ChatRequest
 {
+    /// <summary>
+    /// The id of the model
+    /// </summary>
     [JsonPropertyName("model")]
     public string Model { get; set; } = "";
 
+    /// <summary>
+    /// The list of messages
+    /// </summary>
     [JsonPropertyName("messages")]
     public List<Message> Messages { get; set; } = [];
 
+    /// <summary>
+    /// The temperature (between 0 and 1) to use
+    /// </summary>
     [JsonPropertyName("temperature")]
     public float Temperature { get; set; } = 0.7f;
 
+    /// <summary>
+    /// The max number of tokens to use
+    /// </summary>
     [JsonPropertyName("max_tokens")]
     public int MaxTokens { get; set; } = 4096;
 
+    /// <summary>
+    /// Indicates if the response should be streamed
+    /// </summary>
     [JsonPropertyName("stream")]
     public bool Stream { get; set; } = false;
 }
